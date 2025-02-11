3924fada691388f44f2aa3e455b7aaf1030b19e4
sghobj <sarahghobj@hotmail.com>
2025-02-11 14:33:11 +0100

    landing desktop wip

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,11 +2,16 @@ import './Home.scss'
 
 export const Home = () => {
 
+    const landingImgUrl = 'https://res.cloudinary.com/dsyxohckg/image/upload/v1656873333/Me/masters.jpg'
+    // const landingImgUrl = 'https://res.cloudinary.com/dsyxohckg/image/upload/f_auto,q_auto/v1/Website/landing/laptop-hand'
     return (
 
-        <div className="container-fluid landing-container" >
-            <img className={'img-fluid'} src={'https://res.cloudinary.com/dsyxohckg/image/upload/f_auto,q_auto/v1/Website/landing/laptop-hand'}  alt={'landing-photo'}/>
-        <h1 className={'landing-title'}>Sarah Ghobj</h1>
+        <div className=".container landing-container" >
+            <img className={'img-fluid'} src={landingImgUrl}  alt={'landing-photo'}/>
+            <div className={'landing-text-div'}>
+                <h1 className={'landing-title'}>Sarah Ghobj</h1>
+                <h3 className={'landing-subtitle'}>Software Engineer</h3>
+            </div>
         </div>
 
 
