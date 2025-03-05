45f854b5e8b97d3b3fa26e47a4e4a3531f7b8dd8
sghobj <sarahghobj@hotmail.com>
2025-03-05 23:48:06 +0100

    feature: education and experience

---
+++ src/components/cv/experience/Experience.tsx
@@ -0,0 +1,37 @@
+import './Experience.scss'
+
+export type ExperienceType = {
+    from: string;
+    to?: string;
+    company: string;
+    location: string;
+    position: string;
+    description?: string;
+};
+
+type ExperienceProps = {
+    experiences: ExperienceType[];
+};
+
+export const Experience = ({experiences}: ExperienceProps) => {
+    return (
+        <section>
+                        <div className="table-div">
+                                {experiences.map((item, index) => {
+                                    return (
+                                        <div className={"row border-bottom py-3"} key={index}>
+                                            <div className={'date-col col-12 col-md-3'}>{item.from} - {item.to ? item.to : "now"}</div>
+                                            <div className={'position-col col-12 col-md-6'}>
+                                                <h5 className={'company'}>{item.company}</h5>
+                                                <h6 className={'position'}>{item.position}</h6>
+                                                <p className={'description'}>{item.description ? item.description : ""}</p>
+                                            </div>
+                                            <div className={'location-col col-xs-12 col-md-3 '}>
+                                                {item.location}</div>
+                                        </div>
+                                    )
+                                })}
+                        </div>
+        </section>
+    )
+}
