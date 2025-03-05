3c504b5277c0c27f2fef8fb7d6dd12cf8c4b13fc
sghobj <sarahghobj@hotmail.com>
2025-03-06 00:13:12 +0100

    feature: format date

--- src/components/cv/experience/Experience.tsx
+++ src/components/cv/experience/Experience.tsx
@@ -1,4 +1,5 @@
 import './Experience.scss'
+import {formatDate} from "../../../utils/general.ts";
 
 export type ExperienceType = {
     from: string;
@@ -20,7 +21,7 @@ export const Experience = ({experiences}: ExperienceProps) => {
                                 {experiences.map((item, index) => {
                                     return (
                                         <div className={"row border-bottom py-3"} key={index}>
-                                            <div className={'date-col col-12 col-md-3'}>{item.from} - {item.to ? item.to : "now"}</div>
+                                            <div className={'date-col col-12 col-md-3'}>{formatDate(item.from)} - {item.to ? formatDate(item.to) : "now"}</div>
                                             <div className={'position-col col-12 col-md-6'}>
                                                 <h5 className={'company'}>{item.company}</h5>
                                                 <h6 className={'position'}>{item.position}</h6>
---
+++ src/utils/general.ts
@@ -0,0 +1,14 @@
+export const formatDate = (date: Date | string) => {
+    if (date instanceof Date) {
+        return `${(date.getMonth() + 1).toString().padStart(2, "0")}.${date.getFullYear()}`;
+    } else {
+        {
+            const parsedDate = new Date(date);
+            if (!isNaN(parsedDate.getTime())) {
+                return `${(parsedDate.getMonth() + 1).toString().padStart(2, "0")}.${parsedDate.getFullYear()}`;
+            } else {
+                throw new Error("Invalid date string provided");
+            }
+        }
+    }
+}
