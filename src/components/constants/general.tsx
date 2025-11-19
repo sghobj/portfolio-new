82c528f25616ae19b3508b896cad092e5a02389f
sghobj <sarahghobj@hotmail.com>
2025-11-19 23:31:45 +0100

    feature: add ci pipeline and backend photos in photo gallery
    # Conflicts:
    #	src/components/constants/general.tsx
    #	src/components/ui/provider.tsx
    #	vite.config.ts

---
+++ src/components/constants/general.tsx
@@ -0,0 +1,19 @@
+import { FaHome, FaUser } from "react-icons/fa";
+
+export const links = [
+  {
+    label: "Home",
+    icon: <FaHome className="nav-icon" />,
+    href: "/",
+  },
+  {
+    label: "CV",
+    icon: <FaUser className="nav-icon" />,
+    href: "/cv",
+  },
+  {
+    label: "Showcase",
+    icon: <FaUser className="nav-icon" />,
+    href: "/travel/showcase",
+  },
+];
