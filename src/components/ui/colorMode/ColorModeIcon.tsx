0b6686e4447a2c99222f818eb53fa73e61b913fe
sghobj <sarahghobj@hotmail.com>
2025-11-20 01:26:24 +0100

    feature: add ci pipeline and backend photos in photo gallery

--- src/components/ui/colorMode/ColorModeIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { LuMoon, LuSun } from "react-icons/lu";
-import { useColorMode } from "./useColorMode";
-
-export function ColorModeIcon() {
-  const { colorMode } = useColorMode();
-  return colorMode === "dark" ? <LuMoon /> : <LuSun />;
-}
